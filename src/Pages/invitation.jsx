f8abdff784a03e00104e4504c9c483858a0a1b95
Jeancarlosp94 <jcp9413@gmail.com>
2025-01-30 00:25:41 -0500

    fix image

--- src/Pages/invitation.jsx
+++ src/Pages/invitation.jsx
@@ -11,7 +11,7 @@ const Invitation = () => {
           </div>
           <div className="card-back">
             <p className="message">Sabía que aceptarías! Nos vemos el 14 de febrero a las 7PM. Ponte algo bonito ❤️</p>
-            <img src="/src/assets/img/nico.png" alt="Rosa" className="rose" />
+            <img src="/nico.png" alt="Nico" className="rose" />
             <Link to="/" className="back-btn">Volver al inicio</Link>
           </div>
         </div>
